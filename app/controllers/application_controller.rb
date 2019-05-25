1c89aac8da9ce1f5d46bc6aa66938d40f2b3664c
Richardson William <richardsonwtr@gmail.com>
2019-05-25 18:03:28 -0300

    Adding styles to Admins Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  private
+  
+  # The page layout will be defined by this method, depending on the Controller type
+  # (if it's a Devise Controller or not)
+  def layout_by_resource
+    if devise_controller? # Devise helper that returns true if the Controller it's a Devise
+      "backoffice_devise"
+    else
+      "application"
+    end
+  end
+
 end
