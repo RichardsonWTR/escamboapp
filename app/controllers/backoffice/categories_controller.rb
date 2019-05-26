6e440c3d4201b1f746d41cd7843fc9ef93df63ee
Richardson William <richardsonwtr@gmail.com>
2019-05-26 09:14:19 -0300

    Add: create new category page is implememented

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -4,9 +4,26 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to backoffice_categories_path
+      flash[:notice] = "Categoria \"#{@category.description}\" salva com sucesso"
+    else
+      render "new"
+    end
   end
 
   def edit
+
   end
 
+  private
+
+  def category_params
+    params.require(:category).permit(:description)
+  end
 end
