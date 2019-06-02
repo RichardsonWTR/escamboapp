2c45c38a5defe93404ece8d253518a50f88d5027
Richardson William <richardsonwtr@gmail.com>
2019-06-02 08:21:27 -0300

    Add: Scoping with Pundit

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,10 +1,15 @@
 class Admin < ActiveRecord::Base
+  # Constants
+  ROLES = {:full_access => 0, :restricted_access => 1}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  enum role: [:full_access, :restricted_access]
+  enum role: ROLES
 
-  scope :with_full_access, -> { where(role: 0)}
+  # Scopes
+  scope :with_full_access, -> { where(role: ROLES[:full_access]) }
+  scope :with_restricted_access, -> { where(role: ROLES[:restricted_access]) }
 end
--- app/policies/admin_policy.rb
+++ app/policies/admin_policy.rb
@@ -1,12 +1,18 @@
 class AdminPolicy < ApplicationPolicy
   
+  # Only admins with full access can register a new admin
   def new?
     @user.full_access?
   end
 
   class Scope < Scope
+    # Only admins with full access can see all of the admins
     def resolve
-      scope.all
+      if @user.full_access?
+        scope.all
+      else
+        scope.with_restricted_access
+      end
     end
   end
 end
