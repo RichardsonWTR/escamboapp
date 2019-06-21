878bdb554a4389f3b4996bdc711bc499866e7a05
Richardson William <richardsonwtr@gmail.com>
2019-06-21 19:26:39 -0300

    Fixing up e-mail layout and adding email preview

--- test/mailers/previews/admin_mailer_preview.rb
+++ test/mailers/previews/admin_mailer_preview.rb
@@ -3,4 +3,13 @@ class AdminMailerPreview < ActionMailer::Preview
   def update_email
     AdminMailer.update_email(Admin.first,Admin.last)
   end
+
+  def message_mail
+    @params = {
+      'recipient-text' => Admin.last.email,
+      'subject-text' => 'some subject',
+      'message-text' => 'some message..........'
+    }
+    AdminMailer.message_mail(Admin.first,@params)
+  end
 end
